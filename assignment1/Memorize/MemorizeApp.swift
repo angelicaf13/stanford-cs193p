7dffcfea3537587208171c0713baaa7b0159d8e9
Angelica Figueroa <angelica.figueroa@rockwellcollins.com>
2021-11-03 14:12:34 -0700

    created folder for programming assignment 1

---
+++ assignment1/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Angelica Figueroa Muniz on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
